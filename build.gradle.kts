38c130d631e0137702b4dde3d8e7d2a78fadd3e3
Carlo Marinangeli <carlo.marinangeli@gmail.com>
2023-06-29 17:55:28 +0400

    ukey2 snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ allprojects {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") // sonatype snapshots
     mavenLocal()
   }
 }
